d59aed05efc89ac05353616e750339a8b3b5408d
GangminRYOU <dbrkdals1994@gmail.com>
2023-05-30 17:56:23 +0900

    feat :  add todoDelete PostMapping method to Controller

--- src/main/java/likelion/todolist/controller/ToDoController.java
+++ src/main/java/likelion/todolist/controller/ToDoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,4 +36,10 @@ public class ToDoController {
 		return "redirect:/todo";
 	}
 
+	//서비스 계층에 삭제할 객체의 id를 넘겨준다.
+	@PostMapping("/todo/{todoId}/delete")
+	public String todoDelete(@PathVariable Long todoId){
+		toDoService.deleteToDo(todoId);
+		return "redirect:/todo";
+	}
 }
