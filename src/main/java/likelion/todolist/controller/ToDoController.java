68f1b34c7edd4f6c695f5eeac1e49593c190d780
GangminRYOU <dbrkdals1994@gmail.com>
2023-05-30 17:44:35 +0900

    feat :  add functions to ToDoController which show list of todo and save todo contents to DB

--- src/main/java/likelion/todolist/controller/ToDoController.java
+++ src/main/java/likelion/todolist/controller/ToDoController.java
@@ -1,19 +1,37 @@
 package likelion.todolist.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import likelion.todolist.entity.ToDo;
+import likelion.todolist.service.ToDoService;
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class ToDoController {
+	private final ToDoService toDoService;
+
 	@GetMapping("/todo")
-	@ResponseBody
-	public String helloController(){
-		return "hello";
+	public String helloController(Model model){
+		List<ToDo> toDoList = toDoService.findToDos();
+		model.addAttribute("toDoList", toDoList);
+		return "todolist";
 	}
 
 	@GetMapping("/")
 	public String root(){
+		return "redirect:/todo";
+	}
+	@PostMapping("/todo/create")
+	public String todoCreate(@RequestParam String content){
+		toDoService.saveToDo(content);
 		return "redirect:/todo";
 	}
 
